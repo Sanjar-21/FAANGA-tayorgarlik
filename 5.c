f601f8da9c56f42e018e11586454f0732aa31a13
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-12 23:12:33 +0500

    palindrom

---
+++ 5.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+// Palindrom so'zni aniqlash
+
+int main(void) {
+  char string[] = "asdfdsajk";
+  int middle = strlen(string) / 2;
+  int len = strlen(string);
+
+  printf("middle: %d\n", middle);
+  for (int i = 0; i < middle; i++) {
+    if (string[i] != string[len - i - 1]) {
+      printf("So'z palindrom emas.\n");
+      return 12; // Bir xil bo'lmagan harflar topildi
+    }
+  }
+  printf("So'z palindrom.\n");
+  return 42; // Agar barcha harflar teng bo'lsa
+}
