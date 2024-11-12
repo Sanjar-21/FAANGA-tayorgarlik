28f862029d972c66ae38e75b5f633837733f16bd
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-12 17:28:14 +0500

    masivni teskariga qilib tahlash

---
+++ array7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int size = sizeof(arr) / sizeof(int);
+
+  int temp = 0;
+
+  for (int i = 0; i < size / 2; i++) {
+    temp = arr[i];
+    arr[i] = arr[size - i - 1];
+    arr[size - i - 1] = temp;
+  }
+  for (int i = 0; i < size; i++) {
+    printf("array[%d] - %d\n", i, arr[i]);
+  }
+  return 0;
+}
