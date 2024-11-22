ca432d0cdf33035f09c3fe5dbe0ed1fedc1caed9
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-22 22:55:43 +0500

    kamisya yili aniqlash

--- 15.c
+++ 15.c
@@ -4,9 +4,17 @@
 bool is_leap_year(int year); 
 
 int main(void) {
-    if(is_leap_year(2004)) {
-        printf("Year 2000 is a leap year\n");
-    } else printf("2000 is not a leap year\n");
+    // if(is_leap_year(2004)) {
+    //     printf("Year 2000 is a leap year\n");
+    // } else printf("2000 is not a leap year\n");
+    for(int y = 2004; y <= 2024; y++) {
+        if(is_leap_year(y)) {
+            printf("%d kamisya yil\n", y);
+        }else {
+            printf("%d\n", y);
+        }
+    }
+
     return 0;
 }
 
