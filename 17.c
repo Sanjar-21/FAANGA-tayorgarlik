073e3d31b95489d897b0445527abe693d883db8d
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-23 20:05:42 +0500

    masivlarni birga qushish

--- 17.c
+++ 17.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
 void vector_add(float v1[], float v2[], float result[], int lenght);
+// dot product example
+//
+//  v1 = (2, 5, 4)
+//  v2 = (3, 2, 1)
+//  r = (2 * 3) + (5 * 2) + (4 * 1) = 20 
+//
 
+/*******************************************************************************
+*
+* Program: Dot product
+* 
+* C buycha 17 
+* masivnlarni bir ga qushish 
+*
+* YouTube Lesson: https://www.youtube.com/watch?v=cJybIb_JvuY 
+*
+*
+*******************************************************************************/
 int main(void) {
 
   float v1[] = {2, 5, 4};
