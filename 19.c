367af896b7a1fedf87111e315cb7699aa5168756
Sanjar-21 <oshiqdev1101@gmail.com>
2024-11-25 11:47:21 +0500

    dars 19

---
+++ 19.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int word_countr(char *strig, char *word)   ;
+
+int main(void) {
+
+    return 0;
+}
+
+int word_countr(char *strig, char *word)  {
+    int slen = strlen(strig);
+    int wlen = strlen(word);
+    int end = slen - wlen + 1;
+
+    int cout = 0;
+    for(int i = 0; i < end; i++) {
+        
+    }
+}
