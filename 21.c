65b2cea3675707d65a1303c07b7633fbb048f209
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-02 18:01:03 +0500

    2 likni 10 likga uzgartirish

--- 21.c
+++ 21.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+// Dars 21
+// 01101 larni 10 lik sanoq tizimiga utakazish
 
+int convert(char *sss);
 
 int main(void) {
-	
+  char a1[] = "10101";
+  printf("a1: %d\n", convert(a1));
+
+  return 0;
+}
+int convert(char *sss) {
+  int slen = strlen(sss); // sss ni uzinligini beradi
+  int total = 0;
+  int decval = 1;
+
+  for (int i = (slen - 1); i >= 0; i--) {
+    if (sss[i] == '1') {
+      total = total + decval;
+    }
+    printf("decval: %d\n", decval);
+    decval = decval * 2;
+  }
+  return total;
 }
