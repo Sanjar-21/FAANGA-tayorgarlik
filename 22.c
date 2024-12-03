0ac3dbf707c9a6a1c8897f25d5cc26baf3972788
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-03 21:36:07 +0500

    masivni almashtiramiz indixislarni

--- 22.c
+++ 22.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+void almashtiramiz(char arr[], int arrSize, int index, char ga);
+
 int main(void) {
 	
+  
 	return 0;
 }
+void almashtiramiz(char arr[], int arrSize, int index, char ga) {
+  for(int i  = 0; i < arrSize; i++) {
+    if(arr[i] == index) {
+      arr[i] = ga;
+    }
+  }
+}}
