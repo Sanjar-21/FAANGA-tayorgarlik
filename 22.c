bbaf8e330978caa206555f6a8710edb29e3aa0b3
Sanjar-21 <oshiqdev1101@gmail.com>
2024-12-03 21:52:06 +0500

    char arrayni hegan hariflarni alamashtirish

--- 22.c
+++ 22.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+// C //
+// char dagi suzlarni bironta belgiga almashtirish //
+// YouTube //
 
-void almashtiramiz(char arr[], int arrSize, int index, char ga);
+void almashtiramiz(char *string, char replase, char new);
 
 int main(void) {
-	
-  
-	return 0;
+  char a1[] = "asdfasdfadfasdfasdfsdfsdfsdfsas";
+  almashtiramiz(a1, 'f', ' ');
+  printf("a1: %s\n", a1);
+
+  char a2[] = "asdfasdfadfasdfasdfsdfsdfsdfsas";
+  almashtiramiz(a2, 'f', 'A');
+  printf("a2: %s\n", a2);
+
+  char a3[] = "asdfasdfadfasdfasdfsdfsdfsdfsas";
+  almashtiramiz(a3, 'f', '-');
+  printf("a3: %s\n", a3);
+
+  return 0;
 }
-void almashtiramiz(char arr[], int arrSize, int index, char ga) {
-  for(int i  = 0; i < arrSize; i++) {
-    if(arr[i] == index) {
-      arr[i] = ga;
+
+void almashtiramiz(char *string, char replase, char new) {
+  int slen = strlen(string);
+
+  for (int i = 0; i < slen; i++) {
+
+    if (string[i] == replase) {
+      string[i] = new;
     }
   }
-}}
+}
