9444d134b30d647fd9d7baa01c13d50193649629
Sanjar-21 <oshiqdev1101@gmail.com>
2025-01-03 12:57:15 +0500

    LeetCode 771 py

---
+++ python/leetcode-771.py
@@ -0,0 +1,10 @@
+# # link
+# # https://leetcode.com/problems/jewels-and-stones/description/
+# # code:
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        num = 0
+        for i in stones:
+            if i in jewels:
+                num += 1
+        return num
